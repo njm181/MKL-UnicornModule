9a089869c28f98122757f8d2d80b31547b6626cb
Nicolas Molina <nicolasjmolina1@gmail.com>
2024-08-08 09:12:14 -0300

    First step: Project creation

---
+++ MKL-UnicornModule/MKL_UnicornModule.h
@@ -0,0 +1,18 @@
+//
+//  MKL_UnicornModule.h
+//  MKL-UnicornModule
+//
+//  Created by Nico Molina on 08/08/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MKL_UnicornModule.
+FOUNDATION_EXPORT double MKL_UnicornModuleVersionNumber;
+
+//! Project version string for MKL_UnicornModule.
+FOUNDATION_EXPORT const unsigned char MKL_UnicornModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MKL_UnicornModule/PublicHeader.h>
+
+
